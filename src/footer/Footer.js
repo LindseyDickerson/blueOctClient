d6046c5a0ef01556d476749b5e16a5d4029da74a
Lindsey Dickerson <lindseydickerson2@gmail.com>
2019-11-18 15:43:20 -0500

    footer fix

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,30 +1,58 @@
 import React, {useState} from 'react';
 import { Row, Button, Modal, ModalHeader, ModalBody, ModalFooter  } from 'reactstrap';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 import './footer.css';
 
 
+// const Footer = (props) => {
+
+//     // const {
+//     //     buttonLabel,
+//     //     className
+//     //   } = props;
+    
+//     //   const [modal, setModal] = useState(false);
+    
+//     //   const toggle = () => setModal(!modal);
+
+//     return (
+//         <footer>
+//             {/* <hr id="footerBar"/> */}
+//             <Row>
+//                 <div id="footerSidebar"></div>
+//                 <p id='closingTextName'>©Lindsey Dickerson 2019</p>
+//                 <br/>
+//                 <p id='closingTextSchool'>Eleven Fifty Academy Blue Badge Project</p>
+//             </Row>
+//         </footer>
+//     );
+// };
+
+// export default Footer;
+
 const Footer = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    // const {
-    //     buttonLabel,
-    //     className
-    //   } = props;
-    
-    //   const [modal, setModal] = useState(false);
-    
-    //   const toggle = () => setModal(!modal);
-
+    const toggle = () => setIsOpen(!isOpen);
+  
     return (
-        <footer>
-            {/* <hr id="footerBar"/> */}
-            <Row>
-                <div id="footerSidebar"></div>
-                <p id='closingTextName'>©Lindsey Dickerson 2019</p>
-                <br/>
-                <p id='closingTextSchool'>Eleven Fifty Academy Blue Badge Project</p>
-            </Row>
-        </footer>
+      <div>
+        <Navbar id="footer" light expand="md">
+          <NavbarBrand id="closingTextName">©Lindsey Dickerson || <a href="https://elevenfifty.org/" target="_blank" id="footer1150">Eleven Fifty Blue Badge Project</a></NavbarBrand>
+        </Navbar>
+      </div>
     );
-};
-
-export default Footer;
+  }
+  
+  export default Footer;
